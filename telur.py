68d03e3b48729f1ec9cb1352caf897645a228651
aliance22 <andrealiance33@gmail.com>
2022-05-10 20:05:44 +0700

    Andre

--- telur.py
+++ telur.py
@@ -1,16 +1,20 @@
-class Telur:
+from abc import ABC
+from produk import Produk
+
+class Telur(Produk):
     
-    def set_nama(self, nama : str):
+    def set_nama(self, nama):
         self.__nama = nama
     
     def get_nama(self, nama) ->str:
         return "produk", self.__nama
     
-    def set_harga(self, harga : float):
+    def set_harga(self, harga):
         self.__harga = harga
         
     def get_harga(self, harga)->float:
         return "15000", self.__harga
     
     def show_produk(self):
-        print("produk telur")
+        print(self.__nama)
+        print(self.__harga)
